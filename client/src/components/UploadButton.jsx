0a00a797f2bd74e559a2427417a91865fd6d1ea2
Kartikey Pandey <122464491+kartkp@users.noreply.github.com>
2024-07-21 18:28:35 +0530

    Update UploadButton.jsx

--- client/src/components/UploadButton.jsx
+++ client/src/components/UploadButton.jsx
@@ -23,23 +23,3 @@ const Hero = () => {
 
 export default Hero;
 
-// import React from 'react';
-// import './UploadButton.css';
-
-// const Hero = () => {
-//   const handleRedirect = () => {
-//     window.location.href = './about';
-//   };
-
-//   return (
-//     <div className="containers">
-//       <div className="rectangle">
-//         <button onClick={handleRedirect}>
-//           <span className="button">Go to About Page</span>
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Hero;
